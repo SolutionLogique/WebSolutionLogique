cd828f480cb34568d16e068dd1fda8e4d9518f04
Solution Logique Dev <ebp@solution-logique.fr>
2022-03-28 17:36:11 +0200

    NPM Install multilevel - Components App / Footer/ Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import Navbar from "./components/navbar/Navbar";
+import Home from "./components/home/Home";
 import "./app.scss";
+import Footer from "./components/footer/Footer";
 
 function App() {
   return (
     <div className="app">
+      <Navbar />
       <div className="container">
-        <Navbar />
+        <Home />
         <div className="sections">
           
         </div>
       </div>
+      <Footer />
     </div>
   );
 }
---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,9 @@
+import "./footer.scss"
+
+function Footer() {
+  return (
+    <div className="footer_solution_logique">Copyright © 2022 - Solution Logique Informatique,</div>
+  )
+}
+
+export default Footer
