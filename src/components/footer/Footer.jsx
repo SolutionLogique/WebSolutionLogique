32636f5ba123344a562270bf55a0d8e4509b6a15
Solution Logique Dev <ebp@solution-logique.fr>
2022-03-30 16:30:02 +0200

    Footer - Init footer - Add Information Footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,9 +1,32 @@
-import "./footer.scss"
+import "./footer.scss";
 
 function Footer() {
   return (
-    <div className="footer_solution_logique">Copyright © 2022 - Solution Logique Informatique,</div>
-  )
+    <div className="footer">
+      <div className="footer_global">
+        <div className="footer_copyright_solution_logique">
+          <p>
+            <span>Adresse :</span>
+          </p>
+          <p>475 Rte des Vernes</p>
+          <p>74370 Annecy</p>
+        </div>
+        <div className="footer_copyright_solution_logique">
+          <p>
+            <span>Copyright © 2022 - Solution Logique Informatique</span>
+          </p>
+          <p>Conditions Générales de Vente</p>
+        </div>
+        <div className="footer_copyright_solution_logique">
+          <p>
+            <span>Horaires :</span>
+          </p>
+          <p>Du Lundi au Vendredi</p>
+          <p>8h30 - 12h00 / 14h00 - 18h00</p>
+        </div>
+      </div>
+    </div>
+  );
 }
 
-export default Footer
+export default Footer;
