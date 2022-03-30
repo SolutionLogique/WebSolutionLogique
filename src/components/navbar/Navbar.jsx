95b8c3f990402fac5c9012a5d2b25e63858bdefd
Solution Logique Dev <ebp@solution-logique.fr>
2022-03-30 16:29:29 +0200

    Animation hover - Hover on a href

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,23 +1,29 @@
-import "./navbar.scss"
-import {
-  Link
-} from "react-router-dom";
+import "./navbar.scss";
+import { Link } from "react-router-dom";
 
 function Navbar() {
-  
   return (
-    <div className="navigation_bar">
+    <nav className="navigation_bar">
+      <Link to="/">
         <img alt="logo_solution_logique_informatique" href="" src="assets/logo.png"></img>
-        <li> <Link to="/">Acceuil</Link></li>
-        <li><Link to="/Society">Notre Société</Link></li>
-        <li><Link to ="/logicielsDev">Logiciel & Développement</Link></li>
-        <li><Link to ="/materielReseau">Matériel & Réseau</Link></li>
-        <li><Link to="/contact">Contactez nous</Link></li>
-    </div>
-
-);
-
-
+      </Link>
+      <li>
+        <Link to="/">Accueil</Link>
+      </li>
+      <li>
+        <Link to="/Society">Notre Société</Link>
+      </li>
+      <li>
+        <Link to="/logicielsDev">Logiciel & Développement</Link>
+      </li>
+      <li>
+        <Link to="/materielReseau">Matériel & Réseau</Link>
+      </li>
+      <li>
+        <Link to="/contact">Contactez nous</Link>
+      </li>
+    </nav>
+  );
 }
 
-export default Navbar
+export default Navbar;
