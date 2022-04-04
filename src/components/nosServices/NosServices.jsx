b446a544f2adb07590af7423825b8bef6c107e3d
Solution Logique Dev <ebp@solution-logique.fr>
2022-04-04 17:07:21 +0200

    Install Jquery - Create Tabs

--- src/components/nosServices/NosServices.jsx
+++ src/components/nosServices/NosServices.jsx
@@ -1,36 +1,90 @@
-import "./services.scss"
+import "./services.scss";
+import $ from "jquery";
 
 function NosServices() {
+  $(".tab").click(function () {
+    $(".tab").removeClass("actif");
+    $(this).addClass("actif");
+    $(".content").removeClass("actif");
+  });
+
+  $("#tab1").click(function () {
+    $("#content1").addClass("actif");
+  });
+
+  $("#tab2").click(function () {
+    $("#content2").addClass("actif");
+  });
+
+  $("#tab3").click(function () {
+    $("#content3").addClass("actif");
+  });
+
+  $("#tab4").click(function () {
+    $("#content4").addClass("actif");
+  });
 
   return (
-    
-
     <div className="container">
-		
-		<div id="panneau">
-			<ul id="onglets" className="clearfix">
-				<li >Contenu 1</li>
-				<li >Contenu 2</li>
-				<li >Contenu 3</li>
-			</ul>
-			<ul id="contenus">
-				<li >
-					<p>Tu comprends, même si on frime comme on appelle ça en France... il faut se recréer... pour recréer... a better you et finalement tout refaire depuis le début. Et j'ai toujours grandi parmi les chiens. </p>
-				</li>
-				<li >
-					<p>Si je t'emmerde, tu me le dis, je suis mon meilleur modèle car le cycle du cosmos dans la vie... c'est une grande roue et cela même si les gens ne le savent pas ! Et là, vraiment, j'essaie de tout coeur de donner la plus belle réponse de la terre !  </p>
+      <div className="services_tabs">
+        <div className="tabs">
+          <div className="tab actif" id="tab1">
+            Logiciels
+          </div>
+          <div className="tab" id="tab2">
+            Téléphonie IP
+          </div>
+          <div className="tab" id="tab3">
+            Vidéo IP
+          </div>
+          <div className="tab" id="tab4">
+            Site Internet
+          </div>
+        </div>
+        <div className="content actif" id="content1">
+        <h1 className="title_content">Logiciels</h1>
+        <p>
+          Dignissim integer ut convallis nisl bibendum parturient vivamus leo
+          sociis malesuada consectetur nullam a tincidunt ullamcorper a eros
+          molestie phasellus nec nostra velit mi leo orci a convallis
+          torquent.Nunc a dapibus mi hac at a libero eu lectus inceptos a
+          viverra at feugiat.
+        </p>
+      </div>
+      <div className="content" id="content2">
+        <h1 className="title_content">Téléphonie IP</h1>
+        <p>
+          Dignissim integer ut convallis nisl bibendum parturient vivamus leo
+          sociis malesuada consectetur nullam a tincidunt ullamcorper a eros
+          molestie phasellus nec nostra velit mi leo orci a convallis
+          torquent.Nunc a dapibus mi hac at a libero eu lectus inceptos a
+          viverra at feugiat.
+        </p>
+      </div>
+      <div className="content" id="content3">
+        <h1 className="title_content">Vidéo IP</h1>
+        <p>
+          Dignissim integer ut convallis nisl bibendum parturient vivamus leo
+          sociis malesuada consectetur nullam a tincidunt ullamcorper a eros
+          molestie phasellus nec nostra velit mi leo orci a convallis
+          torquent.Nunc a dapibus mi hac at a libero eu lectus inceptos a
+          viverra at feugiat.
+        </p>
+      </div>
+      <div className="content" id="content4">
+        <h1 className="title_content">Site Internet</h1>
+        <p>
+          Dignissim integer ut convallis nisl bibendum parturient vivamus leo
+          sociis malesuada consectetur nullam a tincidunt ullamcorper a eros
+          molestie phasellus nec nostra velit mi leo orci a convallis
+          torquent.Nunc a dapibus mi hac at a libero eu lectus inceptos a
+          viverra at feugiat.
+        </p>
+      </div>
+      </div>
 
-				</li>
-				<li >
-					<p>You see, premièrement, il faut toute la splendeur du aware et cette officialité peut vraiment retarder ce qui devrait devenir... Donc on n'est jamais seul spirituellement !  </p>
-					<p>Oui alors écoute moi, après il faut s'intégrer tout ça dans les environnements et c'est un très, très gros travail et c'est très, très beau d'avoir son propre moi-même ! Et j'ai toujours grandi parmi les chiens.  </p>
-				</li>
-			</ul>
-</div>
     </div>
-
-
-  )
+  );
 }
 
-export default NosServices
+export default NosServices;
